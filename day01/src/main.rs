9b1483761a787532b58729f8c3a5f5be55c283bf
obscurity <z@x4.pm>
2022-12-01 23:28:58 +0200

    111

---
+++ day01/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let input = include_str!("../input.txt");
+    let elves = input.split("\n\n");
+    let mut calories = Vec::<u32>::new();
+
+    for elf in elves {
+        let val = elf
+            .split('\n')
+            .filter(|s| !s.is_empty())
+            .map(|n| n.parse::<u32>().unwrap())
+            .sum::<u32>();
+        calories.push(val);
+    }
+    calories.sort();
+    println!("part 1: {}", calories.iter().max().unwrap());
+    println!("part 2: {}", calories.iter().rev().take(3).sum::<u32>());
+}
