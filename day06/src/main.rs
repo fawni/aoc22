7e9471e1c20bd62dd8f722448829215b00bb48d2
obscurity <z@x4.pm>
2022-12-06 08:02:32 +0200

    d6: forgot to change this back

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -2,11 +2,11 @@ use std::collections::HashSet;
 
 fn find_unique(chars: &Vec<char>, marker: usize) -> usize {
     for idx in 0..chars.len() {
-        let mut buffer: Vec<char> = Vec::new();
+        let mut buffer: HashSet<char> = HashSet::new();
         for char in chars.iter().skip(idx).take(marker) {
-            buffer.push(*char);
+            buffer.insert(*char);
         }
-        if buffer.into_iter().collect::<HashSet<char>>().len() == marker {
+        if buffer.len() == marker {
             return idx + marker;
         }
     }
